cc79f165037c442c1a4110d9edb75f8a6d991455
JHuelsebusch <jhuelsebusch@gmx.de>
2023-03-28 09:52:43 +0200

    update keyboard wasd

--- js/game.js
+++ js/game.js
@@ -10,16 +10,16 @@ function init() {
 
 }
 window.addEventListener("keydown", (e) => {
-    if (e.keyCode == 39) {
+    if (e.keyCode == 68) {
         keyboard.RIGHT = true;
     }
-    if (e.keyCode == 37) {
+    if (e.keyCode == 65) {
         keyboard.LEFT = true;
     }
-    if (e.keyCode == 38) {
+    if (e.keyCode == 87) {
         keyboard.UP = true;
     }
-    if (e.keyCode == 40) {
+    if (e.keyCode == 83) {
         keyboard.DOWN = true;
     }
     if (e.keyCode == 32) {
@@ -27,16 +27,16 @@ window.addEventListener("keydown", (e) => {
     }
 })
 window.addEventListener("keyup", (e) => {
-    if (e.keyCode == 39) {
+    if (e.keyCode == 68) {
         keyboard.RIGHT = false;
     }
-    if (e.keyCode == 37) {
+    if (e.keyCode == 65) {
         keyboard.LEFT = false;
     }
-    if (e.keyCode == 38) {
+    if (e.keyCode == 87) {
         keyboard.UP = false;
     }
-    if (e.keyCode == 40) {
+    if (e.keyCode == 83) {
         keyboard.DOWN = false;
     }
     if (e.keyCode == 32) {
